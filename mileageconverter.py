83ba85c28a54a02bb0c5a0afddb64911bb5bdb3a
ilijasa <ilijasa@maestralsolutions.com>
2019-12-27 10:53:09 +0100

    typo fix

--- mileageconverter.py
+++ mileageconverter.py
@@ -2,6 +2,6 @@
 
 print(f"Hello there,\nhow many miles would you like to convert to kilometers?")
 miles = input()
-kilometars = int(miles) * 1.60934
+kilometers = int(miles) * 1.60934
 
-print(f"\n{miles} miles is {kilometars} kilometers")
+print(f"\n{miles} miles is {kilometers} kilometers")
