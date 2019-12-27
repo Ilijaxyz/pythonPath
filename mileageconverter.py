6ec8a5e2a7ea5ef1f95dc71de181eae282f6d577
ilijasa <ilijasa@maestralsolutions.com>
2019-12-27 11:09:51 +0100

    rounding kilometers

--- mileageconverter.py
+++ mileageconverter.py
@@ -4,4 +4,4 @@ print(f"Hello there,\nhow many miles is your next ultra trail?")
 miles = input() # input returns a string from the user input
 kilometers = float(miles) * 1.60934 # int would chop off decimals and reduce precison
 
-print(f"\nCool, your ultra trail of {miles} miles is in kilometers {kilometers} km long")
+print(f"\nCool, your ultra trail of {miles} miles is in kilometers {round(kilometers,2)} km long")
