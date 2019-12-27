6250d88c9e685154213d32af9c6445558b991952
ilijasa <ilijasa@maestralsolutions.com>
2019-12-27 10:50:53 +0100

    small mileage converter

---
+++ mileageconverter.py
@@ -0,0 +1,7 @@
+''' Converter miles to kilometers '''
+
+print(f"Hello there,\nhow many miles would you like to convert to kilometers?")
+miles = input()
+kilometars = int(miles) * 1.60934
+
+print(f"\n{miles} miles is {kilometars} kilometers")
--- strings.py
+++ strings.py
@@ -1,12 +1,49 @@
-#Strings, nameing conventions
+#Learning experience of strings operations
 
 x = 100
 dragons = 1
-print(dragons + x)
+#print(dragons + x)
 
 cash = 599888333.44
-print(cash/5)
+#print(cash/5)
 
 ''' Variable nameing convention letter or underscore to start the name '''
 ''' Use snake_case '''
 ''' ALL caps for constants '''
+
+
+''' Dynamic typing showcase '''
+awesome = True
+#print(awesome)
+
+awesome = "a dog"
+#print(awesome)
+
+awesome = None # Concept of nothing in Pyton
+#print(awesome)
+
+awesome = 22 / 7
+#print(awesome)
+
+'''Escape characters all start with backslash'''
+''' \n new line'''
+
+message = "he said \"hey\""
+#print(message)
+
+mountain = "/\\"
+#print(mountain)
+
+''' Formating strings '''
+''' Interpolation and F strings there is also .format method'''
+lucky_number = 10
+msg = f"my {lucky_number}"
+#print(msg)
+
+decimal = 2.3434322
+integer = int(decimal) # it dosen't round just chops off decimal numbers
+print(integer)
+
+numbers = [2, 3, 4]
+number_string = str(numbers)
+print(number_string)
