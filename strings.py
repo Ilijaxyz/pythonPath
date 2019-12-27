66fa9eae5f183f4ce2903d347b9ebfef8a4e5b31
ilijasa <ilijasa@maestralsolutions.com>
2019-12-27 08:35:43 +0100

    nums and strings

---
+++ strings.py
@@ -0,0 +1,12 @@
+#Strings, nameing conventions
+
+x = 100
+dragons = 1
+print(dragons + x)
+
+cash = 599888333.44
+print(cash/5)
+
+''' Variable nameing convention letter or underscore to start the name '''
+''' Use snake_case '''
+''' ALL caps for constants '''
