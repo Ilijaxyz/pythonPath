568917fe70efb4a31667f831f6a374638d4e3a97
ilijasa <ilijasa@maestralsolutions.com>
2020-01-16 13:25:11 +0100

    resolved pipenv issues

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
 import sys
+import requests
 
 print(sys.version)
 print(sys.executable) #where located on my computer
 
+res = requests.get("https://news.ycombinator.com/")
 print("Hello")
+print(res)
